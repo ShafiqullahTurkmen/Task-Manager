9efd49eef876043f0eac3b7e7cd409105411a229
shafiqullah turkmen <shafiqullahturkmen@gmail.com>
2021-12-16 13:22:25 +0300

    Error handling added

--- app.js
+++ app.js
@@ -1,20 +1,25 @@
 const express = require("express");
-const app = express();
-const tasks = require("./routes/tasks");
 const connectDB = require("./db/connect");
 require('dotenv').config()
+const tasks = require("./routes/tasks");
+const notFound = require('./middlewares/not-found')
+const errorHandlerMiddleware = require('./middlewares/error-handler')
+
+
+
+const app = express();
 
 //middleware
+app.use(express.static('./public'))
 app.use(express.json());
 
 //routes
-app.get("/", (req, res) => {
-  res.send("Task Manager App");
-});
 
 app.use("/api/v1/tasks", tasks);
+app.use(notFound)
+app.use(errorHandlerMiddleware)
 
-const port = 3000;
+const port = process.env.PORT || 3000;
  
 const start = async () => {
   try {
