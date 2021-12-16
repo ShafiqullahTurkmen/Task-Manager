4508f7b1004c269601e61216bca95051c1d05219
shafiqullah turkmen <shafiqullahturkmen@gmail.com>
2021-12-16 10:18:24 +0300

    Task model created and models folder added

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,13 +1,25 @@
+const Task = require('../models/Task')
 
+const getAllTasks = async (req, res) => {
+    try {
+        const tasks = await Task.find({})
+        res.status(200).json({tasks})
 
-const getAllTasks = (req, res) => {
-    res.send('get All tasks')
+    } catch (error) {
+        res.status(500).json({msg: error})
+    }
 }
 
-const createTask = (req, res) => {
-    res.json(req.body);
+const createTask = async (req, res) => {
+    try {
+        const task = await Task.create(req.body);
+        res.status(201).json({task});
+    } catch (error) {
+        res.status(500).json({msg: error})
+    }
+   
 }
-
+ 
 const getTask = (req, res) => {
     res.json({id: req.params.id})
 }
